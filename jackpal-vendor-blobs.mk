d848f765366660ccc2a0a8c53dd3da5f4ee3fff7
Paul Beeler <sparksco@gmail.com>
2012-09-05 21:48:22 -0600

    jackpal's Android Terminal Emulator:
    Add the market version here so we can update from the market.
    Also so we don't need vendor/cm/get-prebuilts script anymore...
    
    Signed-off-by: Paul Beeler <sparksco@gmail.com>

---
+++ jackpal-vendor-blobs.mk
@@ -0,0 +1,3 @@
+PRODUCT_COPY_FILES +=  \
+    vendor/jackpal/proprietary/app/Term.apk:system/app/Term.apk \
+    vendor/jackpal/proprietary/lib/libjackpal-androidterm4.so:system/lib/libjackpal-androidterm4.so
---
+++ jackpal-vendor.mk
@@ -0,0 +1,1 @@
+$(call inherit-product, vendor/jackpal/jackpal-vendor-blobs.mk)
